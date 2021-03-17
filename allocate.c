4876c6d6b8889e9d2a1732ccbb3d04f8dedda7df
Yiyang <yiyahuang@student.edu.au>
2021-03-17 13:46:46 +1100

    Added allocate and makefile

---
+++ allocate.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    int file_index, num_processor = 0;
+
+    for(int i = 1; i < argc; i++) {
+        if(!strcmp(argv[i],"-f")) {
+            file_index = i + 1;
+            
+        }
+        if(!strcmp(argv[i],"-p")) {
+            num_processor = atoi(argv[i+1]);
+        }
+    }
+    char* filename = argv[file_index];
+    
+    // printf("%s, %d\n",filename,num_processor);
+    
+    return 0; 
+}
